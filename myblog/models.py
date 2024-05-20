bf9511fbdef6555a2031af7ff6132211e0a24493
zhou_mfk <zhou_mfk@163.com>
2024-05-20 19:31:25 +0800

    feature: 为模型增加基础公共类

--- myblog/models.py
+++ myblog/models.py
@@ -1,21 +1,63 @@
-from sqlalchemy import ForeignKey, String, Text
+import datetime
+from typing import Any, Dict
+
+from sqlalchemy import ForeignKey, String, Text, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing_extensions import Annotated
 
 from .ext import db
 
+"""设置类型映射"""
 
-class User(db.Model):
+intpk = Annotated[int, mapped_column(primary_key=True)]
+timestamp = Annotated[
+    datetime.datetime,
+    mapped_column(nullable=False, server_default=func.current_timestamp()),
+]
+
+
+class BaseModel(db.Model):
+    """声明基类，用于公共模型, 以及公共查询"""
+
+    __abstract__ = True
+
+    id: Mapped[intpk]
+    created_at: Mapped[timestamp]
+
+    @classmethod
+    def save(cls, data: Dict):
+        new_data = cls(**data)
+        db.session.add(new_data)
+        try:
+            db.session.commit()
+        except Exception:
+            db.session.rollback()
+
+    @classmethod
+    def update(cls, data: Dict):
+        cls.update(**data)
+        db.session.add(cls)
+        try:
+            db.session.commit()
+        except Exception:
+            db.session.rollback()
+
+
+class User(BaseModel):
     __tablename__ = "user"
 
-    id: Mapped[int] = mapped_column(primary_key=True, doc="用户id")
     username: Mapped[str] = mapped_column(String(128), doc="用户", nullable=True)
     email: Mapped[str] = mapped_column(String(128))
 
+    def __init__(self, *kwargs) -> None:
+        super().__init__()
+        self.username = kwargs.get("username")
+        self.email = kwargs.get("email")
 
-class Post(db.Model):
+
+class Post(BaseModel):
     __tablename__ = "post"
 
-    id: Mapped[int] = mapped_column(primary_key=True, doc="文章id")
     user_id = mapped_column(ForeignKey("user.id"))
     title = mapped_column(Text)
     body = mapped_column(Text)
